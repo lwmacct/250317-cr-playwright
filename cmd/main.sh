1554901dcc72f77c2e633f8d852ca6ddc78871b3
lwmacct <lwmacct@icloud.com>
2025-04-23 14:51:13 +0800

    -

--- cmd/main.sh
+++ cmd/main.sh
@@ -4,14 +4,14 @@
 # https://www.yuque.com/lwmacct/shell/declare
 # https://www.yuque.com/lwmacct/terminal/tmux
 
-__x11vnc() {
-  :
-  exec x11vnc -display "$DISPLAY" -passwd "$VNC_PASSWORD" -forever -shared -auth /tmp/xvfb -rfbport "$VNC_PORT"
-}
-
 __xvfb() {
   :
   exec Xvfb "$DISPLAY" -screen 0 "${VNC_RESOLUTION}x${VNC_DEPTH}" -auth /tmp/xvfb
+}
+
+__x11vnc() {
+  :
+  exec x11vnc -display "$DISPLAY" -passwd "$VNC_PASSWORD" -forever -shared -auth /tmp/xvfb -rfbport "$VNC_PORT"
 }
 
 __main() {
@@ -19,7 +19,8 @@ __main() {
   tmux new-session -d -s "tmux" "tail -f /dev/null"
   {
     while true; do
-      for _app in x11vnc xvfb; do
+      for _app in xvfb x11vnc; do
+        echo "$_app"
         tmux new-session -d -s "$_app" "bash /apps/repo/cmd/main.sh $_app"
       done
       sleep 1
@@ -33,19 +34,19 @@ Usage: ${0##*/} [command] [options]
 
 Available commands:
   main       主程序入口
-  x11vnc     启动 x11vnc
   xvfb       启动 xvfb
+  x11vnc     启动 x11vnc
   help       显示帮助信息
 
 Example:
   $0 main
-  $0 x11vnc
   $0 xvfb
+  $0 x11vnc
 EOF
 }
 
 case "$1" in
-main | vmagent | app)
+main | xvfb | x11vnc)
   func="__${1}"
   shift # 移除已解析的命令参数
   $func "$@"
